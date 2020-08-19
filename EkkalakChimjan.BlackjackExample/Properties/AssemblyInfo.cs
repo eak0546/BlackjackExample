5727b748ffdc4adc9ecd2259fc94670b6dff88e9
eak0546 <57831268+eak0546@users.noreply.github.com>
2020-08-20 05:08:41 +0700

    Update AssemblyInfo.cs

--- EkkalakChimjan.BlackjackExample/Properties/AssemblyInfo.cs
+++ EkkalakChimjan.BlackjackExample/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EkkalakChimjan.BlackjackExample")]
+[assembly: AssemblyTitle("Supakit.BlackjackExample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EkkalakChimjan.BlackjackExample")]
+[assembly: AssemblyProduct("Supakit.BlackjackExample")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
